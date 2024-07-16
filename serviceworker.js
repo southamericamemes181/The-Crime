5ea7fd406dcb07849458cb84390cebf7e3212869
Desmondxx2000 <157266806+Desmondxx2000@users.noreply.github.com>
2024-07-15 22:07:25 -0400

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -7,7 +7,7 @@ self.addEventListener("install", function (e) {
 
     caches.open(staticCacheName).then(function (cache) { 
 
-      return cache.addAll(["/"]);
+      return cache.addAll(["/" "asset-v1/app.js"]);
 
     })
 
