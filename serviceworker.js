ebb44ffefd0a97574702b435988d8f3b47b3bc04
Desmondxx2000 <157266806+Desmondxx2000@users.noreply.github.com>
2024-07-16 01:38:20 -0400

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,32 +1,48 @@
-var staticCacheName = "pwa";
- 
+var CACHE_NAME = 'my-pwa';
+var urlsToCache = [
+    '/',
+    '/somePath'
+];
 
-self.addEventListener("install", function (e) {
-
-  e.waitUntil(
-
-    caches.open(staticCacheName).then(function (cache) {
-
-      return cache.addAll(["/"]);
-
-    })
-
-  );
+// Install a service worker
+self.addEventListener('install', event => {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
- 
 
-self.addEventListener("fetch", function (event) {
+// Cache and return requests
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
+});
 
-  console.log(event.request.url);
- 
-
-  event.respondWith(
-
-    caches.match(event.request).then(function (response) {
-
-      return response || fetch(event.request);
-
-    })
-
-  );
+// Update a service worker
+self.addEventListener('activate', event => {
+    var cacheWhitelist = ['wms-pwa'];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
 });
