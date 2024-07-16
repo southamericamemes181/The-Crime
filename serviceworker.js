c73e4ddcb9a3779c1b99782b7b0c1b5555586e4a
Desmondxx2000 <157266806+Desmondxx2000@users.noreply.github.com>
2024-07-15 22:57:41 -0400

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,55 +1,32 @@
-const cacheName = "MyCache_1";
-const precachedResources = ["/", "/app.js";
+var staticCacheName = "pwa";
+ 
 
-async function precache() {
-  const cache = await caches.open(cacheName);
-  return cache.addAll(precachedResources);
-}
+self.addEventListener("install", function (e) {
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(precache());
+  e.waitUntil(
+
+    caches.open(staticCacheName).then(function (cache) {
+
+      return cache.addAll(["/"]);
+
+    })
+
+  );
 });
-async function cacheFirst(request) {
-  const cachedResponse = await caches.match(request);
-  if (cachedResponse) {
-    return cachedResponse;
-  }
-  try {
-    const networkResponse = await fetch(request);
-    if (networkResponse.ok) {
-      const cache = await caches.open("MyCache_1");
-      cache.put(request, networkResponse.clone());
-    }
-    return networkResponse;
-  } catch (error) {
-    return Response.error();
-  }
-}
+ 
 
-self.addEventListener("fetch", (event) => {
-  if (precachedResources.includes(url.pathname)) {
-    event.respondWith(cacheFirst(event.request));
-  }
-});
-function isCacheable(request) {
-  const url = new URL(request.url);
-  return !url.pathname.endsWith(".json");
-}
+self.addEventListener("fetch", function (event) {
 
-async function cacheFirstWithRefresh(request) {
-  const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
-    if (networkResponse.ok) {
-      const cache = await caches.open("MyCache_1");
-      cache.put(request, networkResponse.clone());
-    }
-    return networkResponse;
-  });
+  console.log(event.request.url);
+ 
 
-  return (await caches.match(request)) || (await fetchResponsePromise);
-}
+  event.respondWith(
 
-self.addEventListener("fetch", (event) => {
-  if (isCacheable(event.request)) {
-    event.respondWith(cacheFirstWithRefresh(event.request));
-  }
+    caches.match(event.request).then(function (response) {
+
+      return response || fetch(event.request);
+
+    })
+
+  );
 });
